fe5f4facda188f426bff961ad2fadb3b002a96ab
Vishal Mishra <vishalmishra1249@gmail.com>
2025-04-08 17:43:50 +0530

    2nd commit

--- index.js
+++ index.js
@@ -1,30 +1,65 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-    "h1",
-    {
-        id:"title"
-    },
-    "heading1 from parecl"
-);
+const Title = ()=>{
+    return (
+        <a href="/">
+        <img 
+        className="logo"
+        alt="logo"
+        src="https://i.pinimg.com/474x/e6/17/f1/e617f1bfb9af4d9cf132cd3dec0da072.jpg"/>
+        </a>
+    );
+}
 
-const heading2 =React.createElement(
-    "h2",
-    {
-        id:"title"
-    },
-    "Heading 2"
-);
+const Header = ()=>{
+    return(
+        <div className="header">
+            <Title/>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Cart</li>
 
-const container = React.createElement(
-    "div",
-    {
-        id:"container"
-    },
-    [heading, heading2]
-);
+
+
+                </ul>
+
+            </div>
+
+        </div>
+    );
+
+};
+
+const Body = () =>{
+    return (
+         <h4>Body</h4>    
+    );
+}
+
+const Footer = () =>{
+    return (
+        <h4>Footer</h4>
+    );
+
+}
+
+const AppLAyout = () =>{
+    return (
+        <>
+        <Header/>
+        <Body />
+        <Footer />
+        </>
+
+    );
+}
+
+
 
 
 const root= ReactDOM.createRoot(document.getElementById("root"));
-root.render(container);
+root.render(<AppLAyout />);
